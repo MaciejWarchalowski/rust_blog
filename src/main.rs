532503f370dfa66dfabac76a02242b5f07190f50
Maciej Warchalowski <maciej.warchalowski@wellspring.com>
2020-02-12 22:55:43 -0600

    State machine using structs

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,7 @@ use blog::Post;
 fn main() {
   let mut post = Post::new();
   post.add_text("This is the text of the post");
-  assert_eq!("", post.content());
-
-  post.request_review();
-  assert_eq!("", post.content());
-
-  post.reject();
-  assert_eq!("", post.content());
-
-  post.request_review();
-  post.add_text("This is the text of the post");
-  assert_eq!("", post.content());
-
-  post.approve();
-  assert_eq!("", post.content());
-
-  post.approve();
+  let post = post.request_review();
+  let post = post.approve();
   assert_eq!("This is the text of the post", post.content());
-
 }
